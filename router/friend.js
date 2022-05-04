36353acb566eb701dcc3f1738a9f2dabcdfd2f6b
dlwjdcks11 <dlwjdcks7356@gmail.com>
2022-05-04 05:22:28 +0000

    Delete unnessacary file

--- router/friend.js
+++ router/friend.js
@@ -1,3 +1,39 @@
 const router = require('express').Router();
+const tokenVerify = require('../module/tokenVerify.js');
+const database = requre('../module/database.js');
+
+router.get('/info', async(req, res) => {
+
+});
+
+router.get('', async(req, res) => {
+    const token = req.query.token;
+    const result = {
+        success: false,
+        message: '',
+        friends: [],
+    }
+
+    if (!token) {
+        result.message = '서버 연결 실패. 재시도 해주세요.';
+        return res.send(result);
+    }
+    const verify = await tokenVerify(token);
+
+    if (!verify.success) {
+        result.message = verify.message;
+        return res.send(result);
+    }
+
+    
+});
+
+router.post('', async(req, res) => {
+
+});
+
+router.delete('', async(req, res) => {
+
+});
 
 module.exports = router;
